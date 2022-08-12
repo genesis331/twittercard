aaa9a0abc41ad721732a8583e4838682c8c5752e
genesis331 <cheah3838@gmail.com>
2022-08-12 15:24:05 +0800

    handle invalid tweet ids

--- index.js
+++ index.js
@@ -82,69 +82,73 @@ async function getTweetData(id) {
     let raw = await getResponse(`https://api.twitter.com/2/tweets?ids=${id}&expansions=author_id%2Creferenced_tweets.id%2Creferenced_tweets.id.author_id%2Cattachments.media_keys&tweet.fields=entities%2Csource%2Ccreated_at%2Cpublic_metrics&user.fields=verified%2Cprofile_image_url&media.fields=url%2Cpreview_image_url%2Cheight%2Cwidth`, process.env.TWITTER_AUTH_TOKEN);
     let jsonData = JSON.parse(raw);
     if (validateToken(jsonData)) {
-        let data = {};
-        data.name = jsonData.includes.users[0].name;
-        data.id = "@" + jsonData.includes.users[0].username;
-        data.profpic = (jsonData.includes.users[0].profile_image_url).replace('_normal', '_400x400');
-        data.text = jsonData.data[0].text;
-        if (jsonData.data[0].entities.hashtags) {
-            for (let i = 0; i < jsonData.data[0].entities.hashtags.length; i++) {
-                data.text = translateHashtag(jsonData.data[0].entities.hashtags[i].tag, data.text);
-            }
-        }
-        if (jsonData.data[0].entities.mentions) {
-            for (let i = 0; i < jsonData.data[0].entities.mentions.length; i++) {
-                data.text = translateMentions(jsonData.data[0].entities.mentions[i].username, data.text);
-            }
-        }
-        data.embedurl = null;
-        if (jsonData.data[0].entities.urls) {
-            for (let i = 0; i < jsonData.data[0].entities.urls.length; i++) {
-                if (jsonData.data[0].entities.urls[i].images) {
-                    data.embedurl = jsonData.data[0].entities.urls[i].display_url;
-                    data.embedtitle = jsonData.data[0].entities.urls[i].title;
-                    data.embeddesc = jsonData.data[0].entities.urls[i].description;
-                    data.embedimage = jsonData.data[0].entities.urls[i].images[0].url;
-                }
-                data.text = removeLink(jsonData.data[0].entities.urls[i].url, data.text);
-            }
-        }
-        data.media = jsonData.includes.media ? jsonData.includes.media[0] : null;
-        if (jsonData.includes.tweets) {
-            let quotedData = await getQuotedTweetData(jsonData.includes.tweets[0].id);
-            data.mininame = quotedData.mininame;
-            data.miniid = quotedData.miniid;
-            data.miniprofpic = quotedData.miniprofpic;
-            data.minicontent = quotedData.minicontent;
-            data.entities = quotedData.minientities;
-            if (data.entities.mentions) {
-                for (let i = 0; i < data.entities.mentions.length; i++) {
-                    data.minicontent = translateMentions(data.entities.mentions[i].username, data.minicontent);
+        if (!jsonData.errors) {
+            let data = {};
+            data.name = jsonData.includes.users[0].name;
+            data.id = "@" + jsonData.includes.users[0].username;
+            data.profpic = (jsonData.includes.users[0].profile_image_url).replace('_normal', '_400x400');
+            data.text = jsonData.data[0].text;
+            if (jsonData.data[0].entities.hashtags) {
+                for (let i = 0; i < jsonData.data[0].entities.hashtags.length; i++) {
+                    data.text = translateHashtag(jsonData.data[0].entities.hashtags[i].tag, data.text);
                 }
             }
-            if (data.entities.hashtags) {
-                for (let i = 0; i < data.entities.hashtags.length; i++) {
-                    data.minicontent = translateHashtag(data.entities.hashtags[i].tag, data.minicontent);
+            if (jsonData.data[0].entities.mentions) {
+                for (let i = 0; i < jsonData.data[0].entities.mentions.length; i++) {
+                    data.text = translateMentions(jsonData.data[0].entities.mentions[i].username, data.text);
                 }
             }
-            if (data.entities.urls) {
-                for (let i = 0; i < data.entities.urls.length; i++) {
-                    data.minicontent = removeLink(data.entities.urls[i].url, data.minicontent);
+            data.embedurl = null;
+            if (jsonData.data[0].entities.urls) {
+                for (let i = 0; i < jsonData.data[0].entities.urls.length; i++) {
+                    if (jsonData.data[0].entities.urls[i].images) {
+                        data.embedurl = jsonData.data[0].entities.urls[i].display_url;
+                        data.embedtitle = jsonData.data[0].entities.urls[i].title;
+                        data.embeddesc = jsonData.data[0].entities.urls[i].description;
+                        data.embedimage = jsonData.data[0].entities.urls[i].images[0].url;
+                    }
+                    data.text = removeLink(jsonData.data[0].entities.urls[i].url, data.text);
                 }
             }
-            data.minidate = quotedData.minidate;
-            data.minibluetick = quotedData.minibluetick;
+            data.media = jsonData.includes.media ? jsonData.includes.media[0] : null;
+            if (jsonData.includes.tweets) {
+                let quotedData = await getQuotedTweetData(jsonData.includes.tweets[0].id);
+                data.mininame = quotedData.mininame;
+                data.miniid = quotedData.miniid;
+                data.miniprofpic = quotedData.miniprofpic;
+                data.minicontent = quotedData.minicontent;
+                data.entities = quotedData.minientities;
+                if (data.entities.mentions) {
+                    for (let i = 0; i < data.entities.mentions.length; i++) {
+                        data.minicontent = translateMentions(data.entities.mentions[i].username, data.minicontent);
+                    }
+                }
+                if (data.entities.hashtags) {
+                    for (let i = 0; i < data.entities.hashtags.length; i++) {
+                        data.minicontent = translateHashtag(data.entities.hashtags[i].tag, data.minicontent);
+                    }
+                }
+                if (data.entities.urls) {
+                    for (let i = 0; i < data.entities.urls.length; i++) {
+                        data.minicontent = removeLink(data.entities.urls[i].url, data.minicontent);
+                    }
+                }
+                data.minidate = quotedData.minidate;
+                data.minibluetick = quotedData.minibluetick;
+            } else {
+                data.minicontent = null;
+            }
+            let timestampData = convertTimestamp(jsonData.data[0].created_at);
+            data.time = timestampData.outputTime;
+            data.date = timestampData.outputDate;
+            data.client = jsonData.data[0].source;
+            data.retweets = convertNumberToString(jsonData.data[0].public_metrics.retweet_count);
+            data.likes = convertNumberToString(jsonData.data[0].public_metrics.like_count);
+            data.bluetick = jsonData.includes.users[0].verified;
+            return data;
         } else {
-            data.minicontent = null;
+            return { error: jsonData.errors[0].detail };
         }
-        let timestampData = convertTimestamp(jsonData.data[0].created_at);
-        data.time = timestampData.outputTime;
-        data.date = timestampData.outputDate;
-        data.client = jsonData.data[0].source;
-        data.retweets = convertNumberToString(jsonData.data[0].public_metrics.retweet_count);
-        data.likes = convertNumberToString(jsonData.data[0].public_metrics.like_count);
-        data.bluetick = jsonData.includes.users[0].verified;
-        return data;
     } else {
         console.log('Invalid token provided.');
         process.exit(0);
@@ -243,24 +247,32 @@ app.get('/', (req, res) => {
 
 app.get('/html', async (req, res) => {
     let dataObj = await getTweetData(req.query.id);
-    let card = await generateCard(req, dataObj);
-    res.end(card);
+    if (dataObj.error) {
+        res.status(400).end("Error: " + dataObj.error);
+    } else {
+        let card = await generateCard(req, dataObj);
+        res.end(card);
+    }
 });
 
 app.get('/image', async (req, res) => {
     if (req.query.id) {
         let dataObj = await getTweetData(req.query.id);
-        let card = await generateCard(req, dataObj);
-        const image = await nodeHtmlToImage({
-            html: card,
-            selector: "#twittercard",
-            transparent: true,
-            puppeteerArgs: {
-                args: ['--no-sandbox', '--disable-setuid-sandbox']
-            }
-        });
-        res.type('image/png');
-        res.end(image);
+        if (dataObj.error) {
+            res.status(400).end("Error: " + dataObj.error);
+        } else {
+            let card = await generateCard(req, dataObj);
+            const image = await nodeHtmlToImage({
+                html: card,
+                selector: "#twittercard",
+                transparent: true,
+                puppeteerArgs: {
+                    args: ['--no-sandbox', '--disable-setuid-sandbox']
+                }
+            });
+            res.type('image/png');
+            res.end(image);
+        }
     } else {
         res.end('Please provide a tweet id number.');
     }
